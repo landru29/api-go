320f0985a851275bdda7e8e8a93dd12f7d50d6b3
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-08-19 14:07:46 +0200

    pass parameters

--- api.go
+++ api.go
@@ -1,26 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/landru29/api-go/helpers/config"
-	"github.com/landru29/api-go/helpers/mongo"
+    "fmt"
+
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
 )
 
+var mainCommand = &cobra.Command{
+    Use:   "api-go",
+    Short: "API by noopy",
+    Long:  "Full API by noopy",
+    Run: func(cmd *cobra.Command, args []string) {
+        viper.SetEnvPrefix("noopy")
+        viper.AutomaticEnv()
+        viper.SetConfigType("json")
+        viper.SetConfigName("config")
+        viper.AddConfigPath(".")
+        err := viper.ReadInConfig()
+        if err != nil {
+            fmt.Println(err.Error())
+        }
+
+        // Application statup here
+        fmt.Println(viper.GetString("api_host"))
+    },
+}
+
+func init() {
+    flags := mainCommand.Flags()
+    flags.String("api-host", "your-api-host", "API host")
+    viper.BindPFlag("api_host", flags.Lookup("api-host"))
+}
 
 func main() {
-	fmt.Printf("Launching API\n")
-
-	config.Load()
-	mongo.AutoConnect()
-
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+    mainCommand.Execute()
 
 }
+
+/*
+   mongo.AutoConnect()
+
+   r := gin.Default()
+
+   r.GET("/ping", func(c *gin.Context) {
+       c.JSON(200, gin.H{
+           "message": "pong",
+       })
+   })
+   r.Run()*/

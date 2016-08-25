d665f437ec588440353f09dafe9e1b1379711da6
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-08-25 14:37:40 +0200

    Token: update if exists otherwise insert

--- model/quizz/quizz.go
+++ model/quizz/quizz.go
@@ -1,100 +1,100 @@
 package quizz
 
 import (
-	"math/rand"
+    "math/rand"
 
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 // Choice define a choice in the response
 type Choice struct {
-	Text    string `bson:"text"`
-	Scoring int    `bson:"scoring"`
+    Text    string `bson:"text"`
+    Scoring int    `bson:"scoring"`
 }
 
 // Model define a quizz question
 type Model struct {
-	ID           bson.ObjectId `bson:"_id"`
-	Explaination string        `bson:"explaination"`
-	Image        string        `bson:"image"`
-	Level        int           `bson:"level"`
-	Published    bool          `bson:"published"`
-	Tags         string        `bson:"tags"`
-	Text         string        `bson:"text"`
-	Choices      []Choice      `bson:"choices"`
-	CreatedAt    string        `bson:"createdAt"`
-	UpdatedAt    string        `bson:"updatedAt"`
+    ID           bson.ObjectId `bson:"_id,omitempty"`
+    Explaination string        `bson:"explaination"`
+    Image        string        `bson:"image"`
+    Level        int           `bson:"level"`
+    Published    bool          `bson:"published"`
+    Tags         string        `bson:"tags"`
+    Text         string        `bson:"text"`
+    Choices      []Choice      `bson:"choices"`
+    CreatedAt    string        `bson:"createdAt"`
+    UpdatedAt    string        `bson:"updatedAt"`
 }
 
 func getCollection(db *mgo.Database) *mgo.Collection {
-	return db.C("quizzs")
+    return db.C("quizzs")
 }
 
 // Save function save a question
 func (data Model) Save(db *mgo.Database) (result Model, info *mgo.ChangeInfo, err error) {
-	if data.ID != "" {
-		info, err = getCollection(db).UpsertId(data.ID, bson.M{"$set": data})
-	} else {
-		err = getCollection(db).Insert(data)
-	}
-	result = data
-	return
+    if data.ID != "" {
+        info, err = getCollection(db).UpsertId(data.ID, bson.M{"$set": data})
+    } else {
+        err = getCollection(db).Insert(data)
+    }
+    result = data
+    return
 }
 
 // GetAllPublished function get all published questions
 func GetAllPublished(db *mgo.Database) (results []Model, err error) {
-	results = []Model{}
-	err = getCollection(db).Find(bson.M{"published": true}).All(&results)
-	return
+    results = []Model{}
+    err = getCollection(db).Find(bson.M{"published": true}).All(&results)
+    return
 }
 
 // RandomPublished function get randomize questions in db
 func RandomPublished(db *mgo.Database, count int, level int) (results []Model, err error) {
-	results = []Model{}
-	// get db count
-	total, err := CountPublished(db, level)
-	if err != nil {
-		return
-	}
-	// build the list
-	var buffer []int
-	for i := 0; i < total; i++ {
-		buffer = append(buffer, i)
-	}
-	// random Published
-	for i := 0; i < count; i++ {
-		if len(buffer) > 0 {
-			var element Model
-			index := rand.Intn(len(buffer))
-			skip := buffer[index]
-			buffer = append(buffer[:index], buffer[index+1:]...)
-			getCollection(db).Find(bson.M{"published": true, "level": level}).Skip(skip).Limit(1).One(&element)
-			results = append(results, element)
-		}
-	}
-	return
+    results = []Model{}
+    // get db count
+    total, err := CountPublished(db, level)
+    if err != nil {
+        return
+    }
+    // build the list
+    var buffer []int
+    for i := 0; i < total; i++ {
+        buffer = append(buffer, i)
+    }
+    // random Published
+    for i := 0; i < count; i++ {
+        if len(buffer) > 0 {
+            var element Model
+            index := rand.Intn(len(buffer))
+            skip := buffer[index]
+            buffer = append(buffer[:index], buffer[index+1:]...)
+            getCollection(db).Find(bson.M{"published": true, "level": level}).Skip(skip).Limit(1).One(&element)
+            results = append(results, element)
+        }
+    }
+    return
 }
 
 // Find function find an element
 func Find(db *mgo.Database, id string) (result Model, err error) {
-	result = Model{}
-	err = getCollection(db).FindId(id).One(&result)
-	return
+    result = Model{}
+    err = getCollection(db).FindId(id).One(&result)
+    return
 }
 
 // CountAll count all the questions
 func CountAll(db *mgo.Database) (result int, err error) {
-	result, err = getCollection(db).Count()
-	return
+    result, err = getCollection(db).Count()
+    return
 }
 
 // CountPublished count all published questions
 func CountPublished(db *mgo.Database, level int) (result int, err error) {
-	if level < 0 {
-		result, err = getCollection(db).Find(bson.M{"published": true}).Count()
-	} else {
-		result, err = getCollection(db).Find(bson.M{"published": true, "level": level}).Count()
-	}
-	return
+    if level < 0 {
+        result, err = getCollection(db).Find(bson.M{"published": true}).Count()
+    } else {
+        result, err = getCollection(db).Find(bson.M{"published": true, "level": level}).Count()
+    }
+    return
 }
--- model/token/token.go
+++ model/token/token.go
@@ -1,50 +1,48 @@
 package token
 
 import (
-	"fmt"
-
-	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+    mgo "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 // Model define a user
 type Model struct {
-	ID           bson.ObjectId `bson:"_id"`
-	TokenType    string        `bson:"tokenType"`
-	AccessToken  string        `bson:"accressToken"`
-	RefreshToken string        `bson:"refreshToken"`
-	Origin       string        `bson:"origin"`
-	Email        string        `bson:"email"`
-	Expiry       int64         `bson:"expiry"`
+    ID           bson.ObjectId `bson:"_id,omitempty"`
+    TokenType    string        `bson:"tokenType"`
+    AccessToken  string        `bson:"accressToken"`
+    RefreshToken string        `bson:"refreshToken"`
+    Origin       string        `bson:"origin"`
+    Email        string        `bson:"email"`
+    Expiry       int64         `bson:"expiry"`
 }
 
 func getCollection(db *mgo.Database) *mgo.Collection {
-	return db.C("token")
+    return db.C("token")
 }
 
 // FindToken find a user
 func FindToken(db *mgo.Database, ID string) (result Model, err error) {
-	err = getCollection(db).FindId(bson.M{"_id": bson.ObjectIdHex(ID)}).One(&result)
-	return
+    err = getCollection(db).FindId(bson.M{"_id": bson.ObjectIdHex(ID)}).One(&result)
+    return
 }
 
 // Save save a user
 func (data Model) Save(db *mgo.Database) (result Model, err error) {
-	var count int
-	result = Model{}
-	count, err = getCollection(db).Find(bson.M{"email": data.Email}).Limit(1).Count()
-	fmt.Println(err)
-	if err != nil {
-		return
-	}
-	if count == 0 {
-		data.ID = bson.NewObjectId()
-		err = getCollection(db).Insert(data)
-	} else {
-		data.ID = bson.NewObjectId()
-		err = getCollection(db).Update(bson.M{"email": data.Email}, data)
-	}
-	fmt.Println(err)
-	result = data
-	return
+    result = Model{}
+    tmp := Model{}
+    err = getCollection(db).Find(bson.M{"email": data.Email}).One(&tmp)
+    if err != nil {
+        if err == mgo.ErrNotFound {
+            data.ID = bson.NewObjectId()
+            err = getCollection(db).Insert(data)
+            result = data
+        }
+        return
+    }
+    data.ID = tmp.ID
+    result = data
+
+    err = getCollection(db).Update(bson.M{"email": data.Email}, data)
+
+    return
 }
